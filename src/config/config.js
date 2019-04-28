ecbac8f8f68a090e43072c827b9e192ba2ec493c
nat55555 <nataliacardona72@hotmail.com>
2019-04-27 22:59:14 -0500

    s

--- src/config/config.js
+++ src/config/config.js
@@ -6,10 +6,11 @@ if (process.env.NODE_ENV === 'local'){
 	urlDB = 'mongodb://localhost:27017/nodonode';
 }
 else {
-	//urlDB = 'mongodb+srv://nodejstdea:nodejstdea@nodejstdea-4jn4i.mongodb.net/asignaturas?retryWrites=true'
+
 	urlDB = 'mongodb+srv://admin:nodonode@nodonode-5w6kb.mongodb.net/test?retryWrites=true'
 }
 
 process.env.URLDB = urlDB
 
-process.env.SENDGRID_API_KEY='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';
+
+SENDGRID_API_KEY='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';
